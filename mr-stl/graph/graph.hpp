6500bc06352e9774f7ef1552c2a4c841f0b6ec63
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-03-27 20:42:54 +0300

    Graph reworked for pair of vectors instead of vector of pairs

--- mr-stl/graph/graph.hpp
+++ mr-stl/graph/graph.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include "mr-stl/vector/vector.hpp"
 #include "mr-stl/vector/amortized_vector.hpp"
 
@@ -8,11 +9,15 @@ namespace mr {
     class Graph {
     public:
       using Node = T;
-      using Edge = std::pair<std::size_t, std::size_t>;
+      using Destination = std::size_t;
+      using Edge = std::pair<Destination, Destination>;
       using Path = mr::Vector<Node>;
 
     private:
-      mr::Vector<Edge> _edges;
+      // a flat multimap of edges
+      mr::Vector<Destination> _destinations;
+      mr::Vector<Destination> _destinations_lookup;
+      // stored data
       mr::Vector<Node> _nodes;
 
     public:
@@ -35,6 +40,16 @@ namespace mr {
         return std::nullopt;
       }
 
+      std::optional<std::span<Destination>> node_children(Destination node_dest) {
+        Destination * const beg = _destinations_lookup[node_dest];
+        Destination * const end = _destinations_lookup[node_dest + 1] - 1;
+        return std::span<Destination>(beg, end);
+      }
+
+      std::optional<std::span<Destination>> node_children(const Node &node_val) {
+        return find(node_val).and_then(bind_self(&Graph::node_children, this));
+      }
+
       std::optional<Path> find_path_reversed(std::size_t src, std::size_t dest) const {
         if (src > _nodes.size() || dest > _nodes.size()) {
           return std::nullopt;
@@ -44,21 +59,15 @@ namespace mr {
         }
 
         // find range of edges where src is source
-        const Edge * beg = nullptr;
-        for (const Edge &edge : _edges) {
-          if (edge.first == src) {
-            beg = &edge;
-            break;
-          }
-        }
+        std::span<Destination> children = node_children(src);
+
         // calculate path from each child-node, return minimum
         Path candidate;
-        while (beg->first == src) {
-          auto tmp = find_path_reversed(beg->second, dest);
+        for (auto child : children) {
+          auto tmp = find_path_reversed(child, dest);
           if (tmp.has_value() && (tmp->size() < candidate.size() || candidate.size() == 0)) {
             candidate = std::move(tmp.value());
           }
-          beg++;
         }
 
         return std::move(candidate.emplace_back(src));
@@ -66,25 +75,16 @@ namespace mr {
 
       std::optional<Path> find_path(std::size_t src, std::size_t dest) const {
         auto tmp = find_path_reversed(src, dest);
-        if (tmp.has_value()) {
-          // return std::ranges::reverse(std::move(tmp.value()));
-          std::ranges::reverse(tmp.value());
-          return std::move(tmp.value());
-        }
-        return std::nullopt;
+        return tmp.and_then([](Path &path) {
+            std::ranges::reverse(path);
+            return path;
+            });
       }
 
       template <typename Fn1, typename Fn2>
         requires (std::is_invocable_v<Fn1, Node> && std::is_invocable_v<Fn2, Node>)
       std::optional<Path> find_path(Fn1 &&f1, Fn2 &&f2) const {
-        std::optional<std::size_t> src = find_if(f1);
-        std::optional<std::size_t> dest = find_if(f2);
-
-        if (src.has_value() &&
-            dest.has_value()) [[likely]] {
-          return find_path(src.value(), dest.value());
-        }
-        return std::nullopt;
+        return and_then(find_if(f1), find_if(f2), bind_self(&Graph::find_path, this));
       }
 
       template <typename... Args> requires(std::is_constructible_v<T, Args...>)
@@ -94,30 +94,27 @@ namespace mr {
       }
 
       Graph &add_edge(std::size_t src, std::size_t dest) {
-        _edges.push_sorted(src, dest);
+        std::span<Destination> children = node_children(src);
+        if (contains(children, [dest](auto val) { return val == dest; })) {
+          return *this;
+        }
+
+        _destinations.emplace_at(&*children.end(), dest);
+        for (std::size_t i = src; i < _destinations_lookup.size(); i++) {
+          _destinations_lookup[i]++;
+        }
+
         return *this;
       }
 
       template <typename Func1, typename Func2>
         requires(std::is_invocable_v<Func1, Node> && std::is_invocable_v<Func2, Node>)
       Graph &add_edge(Func1 &&f1, Func2 &&f2) {
-        std::optional<std::size_t> src = find_if(f1);
-        std::optional<std::size_t> dest = find_if(f2);
-
-        if (src && dest) [[likely]] {
-          add_edge(src.value(), dest.value());
-        }
-
-        return *this;
+        return and_then(find_if(f1), find_if(f2), bind_self(&Graph::add_edge, this));
       }
 
       // getters
       mr::Vector<Node> &nodes() noexcept { return _nodes; }
-
       const mr::Vector<Node> &nodes() const noexcept { return _nodes; }
-
-      mr::Vector<Edge> &edges() noexcept { return _edges; }
-
-      const mr::Vector<Edge> &edges() const noexcept { return _edges; }
     };
 }  // namespace mr
