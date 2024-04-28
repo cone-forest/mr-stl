626bfd7d1276d854326d0e6e955a536e495cb7e4
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-04-29 01:43:33 +0300

    Abuse of std::move & remove method

--- mr-stl/vector/vector.hpp
+++ mr-stl/vector/vector.hpp
@@ -13,7 +13,8 @@ namespace mr {
       Vector() noexcept = default;
       ~Vector() noexcept = default;
 
-      template <typename ...Args> requires (std::is_constructible_v<T, Args> && ...)
+      template <typename ...Args>
+        // requires (std::is_constructible_v<T, Args> && ...)
         Vector(Args... args) : _data(static_cast<T>(args)...), _size(sizeof...(args)) {}
 
       Vector(const T *data, std::size_t size) :
@@ -28,12 +29,12 @@ namespace mr {
       Vector & operator=(Vector &&other) noexcept = default;
 
       template <typename ...Args>
-        requires (std::is_constructible_v<T, Args...>)
+        // requires (std::is_constructible_v<T, Args...>)
         Vector & emplace_back(Args ...args) {
           if (_size >= _data.size()) [[unlikely]] {
             _data = resized(_size * 2 + 1).value_or(std::move(_data)); // assign new value on success
           }
-          _data[_size++] = T(args...);
+          _data[_size++] = std::move(T(args...));
           return *this;
         }
 
@@ -53,15 +54,14 @@ namespace mr {
         emplace_back(args...);
         // insertion sort
         std::size_t i = _size - 1; // last element index
-        auto at = [this](auto idx) -> T& { return this->operator[](idx); };
+        auto at = [this](auto idx) -> T& { return (*this)[idx]; };
 
         T val = at(i);
         while (i > 0 && at(i - 1) > val) {
-          at(i) = at(i - 1);
+          at(i) = std::move(at(i - 1));
           i--;
         }
-
-        at(i) = val;
+        at(i) = std::move(val);
 
         return *this;
       }
@@ -73,7 +73,7 @@ namespace mr {
       for (std::size_t i = index + 1; i < size(); i++) {
         _data[i] = _data[i - 1];
       }
-      _data[index] = T(args...);
+      _data[index] = std::move(T(args...));
       return *this;
     }
 
@@ -83,13 +83,26 @@ namespace mr {
       return emplace_back(std::distance(data(), location), args...);
     }
 
+    Vector & remove(std::size_t id) {
+      _size--;
+      for (std::size_t i = id; i < _size; i++) {
+        _data[i] = _data[i + 1];
+      }
+    }
+
+    Vector & clear() {
+      _size = 0;
+    }
+
     // getters
-    T * data() noexcept {return _data.data(); }
-    const T * data() const noexcept {return _data.data(); }
+    T * data() noexcept { return _data.data(); }
+    const T * data() const noexcept { return _data.data(); }
+
     std::size_t size() const noexcept {return _size; }
     std::size_t capacity() const noexcept {return _data.size(); }
+
     T& operator[](std::size_t i) { return _data[i]; }
-    T operator[](std::size_t i) const { return _data[i]; }
+    const T & operator[](std::size_t i) const { return _data[i]; }
 
     bool operator<(const Vector<T> &other) const noexcept {
       return _size < other._size || (_size == other._size && _data < other._data);
