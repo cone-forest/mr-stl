72f4cb3934b0e2b70ad1dc4c4795ab7551c4d723
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-05-29 00:18:53 +0300

    The great BigInt rewrite

--- mr-stl/bigint/bigint.hpp
+++ mr-stl/bigint/bigint.hpp
@@ -1,144 +1,153 @@
+#include <cassert>
+#include <cstdint>
 #include <limits>
 #include "mr-stl/vector/vector.hpp"
 
 namespace mr {
-  // forward declaration
-  struct BigInt;
-  template <typename T> struct PositiveRef;
-  template <typename T> struct NegativeRef;
-
-  template <typename T>
-    struct PositiveRef {
-      const T &ref;
-      PositiveRef(const T &r) : ref(r) {}
-      operator const T &() { return ref; }
-      friend NegativeRef<T> operator-(PositiveRef<T> self) {
-        return self.ref;
-      }
+  template <std::integral T = std::uint64_t>
+  struct UnsignedBigInt {
+    inline static constexpr T shit_max = std::numeric_limits<T>::max();
+    enum class Sign : int {
+      Negative = 0,
+      Positive = 1,
     };
 
-  template <typename T>
-    struct NegativeRef {
-      const T &ref;
-      NegativeRef(const T &r) : ref(r) {}
-      operator const T &() { return ref; }
-      friend PositiveRef<T> operator-(NegativeRef<T> self) {
-        return self.ref;
-      }
-    };
+    // absolute value
+    Vector<T> _value;
+    // sign
+    Sign _sign = Sign::Positive;
 
-  template <typename T>
-    struct SignedRef {
-      const T &ref;
-      bool is_negative = false;
+    UnsignedBigInt() noexcept = default;
+    UnsignedBigInt(Vector<T> value, Sign sign = Sign::Positive) noexcept :
+      _value(std::move(value)), _sign(sign) {}
 
-      SignedRef<T>(const T &r, bool is_neg) : ref(r), is_negative(is_neg) {}
+    UnsignedBigInt(UnsignedBigInt &&other) noexcept = default;
+    UnsignedBigInt & operator=(UnsignedBigInt &&other) noexcept = default;
 
-      SignedRef<T> operator-() const {
-        return SignedRef<T>(*this, !is_negative);
-      }
-    };
+    UnsignedBigInt(const UnsignedBigInt &other) noexcept = default;
+    UnsignedBigInt & operator=(const UnsignedBigInt &other) noexcept = default;
 
-  struct BigInt {
-    inline static constexpr std::uint32_t shit_max = std::numeric_limits<std::uint32_t>::max();
-    using PositiveRefT = PositiveRef<BigInt>;
-    using NegativeRefT = NegativeRef<BigInt>;
-    using SignedRefT = SignedRef<BigInt>;
+    template <std::integral S> UnsignedBigInt(S value) { _value.emplace_back(static_cast<T>(value)); }
 
-    // absolute
-    Vector<std::uint32_t> _value;
+    friend UnsignedBigInt operator+(const UnsignedBigInt &lhs, const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator-(const UnsignedBigInt &lhs, const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator+(const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator-(const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator*(const UnsignedBigInt &lhs, const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator/(const UnsignedBigInt &lhs, const UnsignedBigInt &rhs);
+    friend UnsignedBigInt operator*(const UnsignedBigInt &lhs, T rhs);
+    friend UnsignedBigInt operator/(const UnsignedBigInt &lhs, T rhs);
+    friend UnsignedBigInt operator%(const UnsignedBigInt &lhs, const UnsignedBigInt &rhs);
 
-    BigInt() noexcept = default;
-
-    BigInt(BigInt &&other) noexcept = default;
-    BigInt & operator=(BigInt &&other) noexcept = default;
-
-    BigInt(const BigInt &other) noexcept = default;
-    BigInt & operator=(const BigInt &other) noexcept = default;
-
-    template <std::integral T> BigInt(T value) { _value.emplace_back(value); }
-
-    // addition operators
-    friend PositiveRefT operator+(PositiveRefT lhs, PositiveRefT rhs);
-    friend SignedRefT   operator+(PositiveRefT lhs, NegativeRefT rhs);
-    friend SignedRefT   operator+(NegativeRefT lhs, PositiveRefT rhs);
-    friend NegativeRefT operator+(NegativeRefT lhs, NegativeRefT rhs);
-
-    // subtraction operators
-    friend SignedRefT   operator-(PositiveRefT lhs, PositiveRefT rhs);
-    friend PositiveRefT operator-(PositiveRefT lhs, NegativeRefT rhs);
-    friend NegativeRefT operator-(NegativeRefT lhs, PositiveRefT rhs);
-    friend SignedRefT   operator-(NegativeRefT lhs, NegativeRefT rhs);
-
-    // multiplication operators
-    friend PositiveRefT operator*(PositiveRefT lhs, PositiveRefT rhs);
-    friend NegativeRefT operator*(PositiveRefT lhs, NegativeRefT rhs);
-    friend NegativeRefT operator*(NegativeRefT lhs, PositiveRefT rhs);
-    friend PositiveRefT operator*(NegativeRefT lhs, NegativeRefT rhs);
-
-    // division operators
-    friend PositiveRefT operator/(PositiveRefT lhs, PositiveRefT rhs);
-    friend NegativeRefT operator/(PositiveRefT lhs, NegativeRefT rhs);
-    friend NegativeRefT operator/(NegativeRefT lhs, PositiveRefT rhs);
-    friend PositiveRefT operator/(NegativeRefT lhs, NegativeRefT rhs);
-
-    BigInt operator-(PositiveRefT other) {
-      BigInt tmp = *this;
-      bool trail = false;
-
-      for (std::size_t i = 0; i < _value.size(); i++) {
-        bool next_trail = (tmp._value[i] > shit_max - other.ref._value[i]);
-        tmp._value[i] += other.ref._value[i] + static_cast<std::uint32_t>(trail);
-        trail = next_trail;
-      }
-
-      _value.emplace_back(static_cast<std::uint32_t>(trail));
-      return tmp;
+    T operator[](std::size_t i) const { return _value[i]; }
+    T & operator[](std::size_t i) { return _value[i]; }
+    auto operator<(const UnsignedBigInt &other) const noexcept {
+      return _sign == Sign::Negative && other._sign == Sign::Positive ||
+        (_sign == Sign::Positive ? (_value < other._value) : !(_value < other._value));
     }
-
-    std::uint32_t operator[](std::size_t i) const { return _value[i]; }
-    std::uint32_t & operator[](std::size_t i) { return _value[i]; }
   };
 
   // addition operators definitions
-  BigInt::PositiveRefT operator+(BigInt::PositiveRefT lhs, BigInt::PositiveRefT rhs) {
-    BigInt tmp = lhs;
+  template <std::integral T>
+  UnsignedBigInt<T> operator+(const UnsignedBigInt<T> &lhs, const UnsignedBigInt<T> &rhs) {
+    // swap arguments if len(lhs) < len(rhs)
+    if (lhs._value.size() < rhs._value.size()) {
+      return rhs + lhs;
+    }
+    // continue assuming len(lhs) >= len(rhs)
+    // perform addition
+    UnsignedBigInt tmp = lhs;
     bool trail = false;
-
-    for (std::size_t i = 0; i < lhs.ref._value.size(); i++) {
-      bool next_trail = (tmp._value[i] > BigInt::shit_max - rhs.ref._value[i]);
-      tmp._value[i] += rhs.ref._value[i] + static_cast<std::uint32_t>(trail);
+    for (std::size_t i = 0; i < rhs._value.size(); i++) {
+      bool next_trail = (tmp._value[i] > UnsignedBigInt<T>::shit_max - std::max<T>(rhs._value[i], trail));
+      tmp._value[i] += rhs._value[i] + static_cast<T>(trail);
       trail = next_trail;
     }
 
-    tmp._value.emplace_back(static_cast<std::uint32_t>(trail));
-    return BigInt::PositiveRefT(tmp);
-  }
-  BigInt::SignedRefT   operator+(BigInt::PositiveRefT lhs, BigInt::NegativeRefT rhs) {
-    return lhs - BigInt::PositiveRefT(rhs);
-  }
-  BigInt::SignedRefT   operator+(BigInt::NegativeRefT lhs, BigInt::PositiveRefT rhs) {
-    return -(rhs + lhs);
-  }
-  BigInt::NegativeRefT operator+(BigInt::NegativeRefT lhs, BigInt::NegativeRefT rhs) {
-    return -(BigInt::PositiveRefT(lhs) + BigInt::PositiveRefT(rhs));
+    // handle trailing 1
+    if (trail) {
+      if (lhs._value.size() != rhs._value.size()) {
+        tmp[rhs._value.size()] += trail;
+      } else {
+        tmp._value.emplace_back(trail);
+      }
+    }
+
+    return tmp;
   }
 
-  // subtraction operators
-  BigInt::SignedRefT   operator-(BigInt::PositiveRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::PositiveRefT operator-(BigInt::PositiveRefT lhs, BigInt::NegativeRefT rhs);
-  BigInt::NegativeRefT operator-(BigInt::NegativeRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::SignedRefT   operator-(BigInt::NegativeRefT lhs, BigInt::NegativeRefT rhs);
+  template <std::integral T>
+  UnsignedBigInt<T> operator-(const UnsignedBigInt<T> &lhs, const UnsignedBigInt<T> &rhs) {
+    // swap arguments if len(lhs) < len(rhs)
+    if (lhs < rhs) {
+      return -(rhs - lhs);
+    }
 
-  // multiplication operators
-  BigInt::PositiveRefT operator*(BigInt::PositiveRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::NegativeRefT operator*(BigInt::PositiveRefT lhs, BigInt::NegativeRefT rhs);
-  BigInt::NegativeRefT operator*(BigInt::NegativeRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::PositiveRefT operator*(BigInt::NegativeRefT lhs, BigInt::NegativeRefT rhs);
+    // continue assuming len(lhs) >= len(rhs)
+    // perform addition
+    UnsignedBigInt tmp = lhs;
+    bool trail = false;
+    for (std::size_t i = 0; i < rhs._value.size(); i++) {
+      bool next_trail = (tmp._value[i] < std::max<int>(rhs._value[i], rhs._value[i] + trail));
+      tmp._value[i] -= rhs._value[i] + static_cast<T>(trail);
+      trail = next_trail;
+    }
 
-  // division operators
-  BigInt::PositiveRefT operator/(BigInt::PositiveRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::NegativeRefT operator/(BigInt::PositiveRefT lhs, BigInt::NegativeRefT rhs);
-  BigInt::NegativeRefT operator/(BigInt::NegativeRefT lhs, BigInt::PositiveRefT rhs);
-  BigInt::PositiveRefT operator/(BigInt::NegativeRefT lhs, BigInt::NegativeRefT rhs);
+    return tmp;
+  }
+
+  template <std::integral T>
+  UnsignedBigInt<T> operator+(const UnsignedBigInt<T> &rhs) {
+    auto tmp = rhs;
+    return tmp;
+  };
+
+  template <std::integral T>
+  UnsignedBigInt<T> operator-(const UnsignedBigInt<T> &rhs) {
+    auto tmp = rhs;
+    tmp.sign = UnsignedBigInt<T>::Sign(!static_cast<int>(tmp.sign));
+    return tmp;
+  };
+
+  template <std::integral T>
+  UnsignedBigInt<T> operator*(const UnsignedBigInt<T> &lhs, const UnsignedBigInt<T> &rhs) {
+    auto tmp = lhs;
+    return tmp;
+  }
+
+  template <std::integral T>
+  UnsignedBigInt<T> operator*(const UnsignedBigInt<T> &lhs, T rhs) {
+    auto size = lhs.size() + rhs.size();
+    UnsignedBigInt<T> tmp(size, 0);
+    for (int i = 0; i < lhs.size(); i++) {
+        T carry = 0;
+        for (std::size_t j = 0; j < rhs.size() || carry; j++) {
+            T product = tmp[i + j] + lhs[i] * (j < rhs.size() ? rhs[j] : (T)0) + carry;
+            tmp[i + j] = product % (T)10;
+            carry = product / (T)10;
+        }
+    }
+    // remove trailing 0
+    while (tmp[size - 1] == 0) {
+      size--;
+    }
+    return {{tmp._value.data(), size}};
+  }
+
+  template <std::integral T>
+  std::tuple<UnsignedBigInt<T>, T> operator/(const UnsignedBigInt<T> &lhs, T rhs) {
+    auto size = lhs.size();
+    T first = lhs[0];
+    UnsignedBigInt<T> result(size);
+    T remainder = 0;
+    for (int i = lhs.size() - 1; i >= 0; i--) {
+        T val = (remainder * (UnsignedBigInt<T>::shit_max / rhs) + lhs[i] / rhs);
+        remainder = remainder * (UnsignedBigInt<T>::shit_max % rhs) + lhs[i] % rhs;
+        result[i] = val;
+    }
+    if (first == rhs) {
+      size--;
+    }
+    return {{result.data(), size}, remainder};
+  }
 }
