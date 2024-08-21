c1d52e9c8963f9e230c9a006b2b173e4e9647322
Michael Tsukanov (MT6) <73912760+cone-forest@users.noreply.github.com>
2024-08-21 17:25:41 +0300

    Add mr::Vector::resize
    Similar to std::vector::reserve. Renaming is possible in the future.

--- mr-stl/vector/vector.hpp
+++ mr-stl/vector/vector.hpp
@@ -34,84 +34,104 @@ namespace mr {
       template <typename ...Args>
         // requires (std::is_constructible_v<T, Args...>)
         Vector & emplace_back(Args ...args) {
-          auto resized =
-              [this](const std::size_t size) -> std::optional<OwningSpan<T>> {
-            if (T *tmp = new (std::nothrow) T[size]; tmp != nullptr) [[likely]] {
-              // move on successful allocation
-              std::uninitialized_move_n(_data.data(), _data.size(), tmp);
-              return OwningSpan(tmp, size);
-            }
-            return std::nullopt;
-          };
-
-          if (_size >= _data.size()) [[unlikely]] {
-            _data = resized(_size * 2 + 1).value_or(std::move(_data)); // assign new value on success
+          while (_size >= _data.size()) [[unlikely]] {
+            try_resize();
           }
-          T tmp{std::forward<Args>(args)...};
+          T tmp = {std::forward<Args>(args)...};
           _data[_size++] = std::move(tmp);
           return *this;
         }
 
-    template <typename ...Args>
-      requires (std::is_constructible_v<T, Args...>)
-      Vector & push_sorted(Args ...args) {
-        emplace_back(args...);
-        // insertion sort
-        std::size_t i = _size - 1; // last element index
-        auto at = [this](auto idx) -> T& { return (*this)[idx]; };
-
-        T val = at(i);
-        while (i > 0 && at(i - 1) > val) {
-          at(i) = std::move(at(i - 1));
-          i--;
+      template <std::input_iterator It>
+        Vector & move_range(It begin, It end) {
+          std::size_t new_size = std::distance(begin, end);
+          while (_data.size() < new_size) {
+            try_resize();
+          }
+          std::uninitialized_move(begin, end, _data.data());
+          return *this;
         }
-        at(i) = std::move(val);
 
+      template <typename ...Args>
+        requires (std::is_constructible_v<T, Args...>)
+        Vector & push_sorted(Args ...args) {
+          emplace_back(args...);
+          // insertion sort
+          std::size_t i = _size - 1; // last element index
+          auto at = [this](auto idx) -> T& { return (*this)[idx]; };
+
+          T val = at(i);
+          while (i > 0 && at(i - 1) > val) {
+            at(i) = std::move(at(i - 1));
+            i--;
+          }
+          at(i) = std::move(val);
+
+          return *this;
+        }
+
+      template <typename ...Args>
+        requires (std::is_constructible_v<T, Args...>)
+      Vector & emplace_at(std::size_t index, Args ...args) {
+        emplace_back();
+        std::memmove(_data + index, _data + index + 1, size() - index - 1);
+        _data[index] = std::move(T(args...));
         return *this;
       }
 
-    template <typename ...Args>
-      requires (std::is_constructible_v<T, Args...>)
-    Vector & emplace_at(std::size_t index, Args ...args) {
-      emplace_back();
-      std::memmove(_data + index, _data + index + 1, size() - index - 1);
-      _data[index] = std::move(T(args...));
-      return *this;
-    }
-
-    template <typename ...Args>
-      requires (std::is_constructible_v<T, Args...>)
-    Vector & emplace_at(T *location, Args ...args) {
-      return emplace_at(std::distance(data(), location), args...);
-    }
-
-    Vector & remove(std::size_t id) {
-      _size--;
-      for (std::size_t i = id; i < _size; i++) {
-        _data[i] = _data[i + 1];
+      template <typename ...Args>
+        requires (std::is_constructible_v<T, Args...>)
+      Vector & emplace_at(T *location, Args ...args) {
+        return emplace_at(std::distance(data(), location), args...);
       }
-    }
 
-    Vector & clear() {
-      _size = 0;
-    }
+      Vector & remove(std::size_t id) {
+        _size--;
+        for (std::size_t i = id; i < _size; i++) {
+          _data[i] = _data[i + 1];
+        }
+      }
 
-    // getters
-    T * data() noexcept { return _data.data(); }
-    const T * data() const noexcept { return _data.data(); }
+      Vector& resize(std::size_t new_size) {
+        while (_data.size() < new_size) [[unlikely]] {
+          try_resize();
+        }
+      }
 
-    std::size_t size() const noexcept {return _size; }
-    std::size_t capacity() const noexcept {return _data.size(); }
+      Vector & clear() {
+        _size = 0;
+      }
 
-    T& operator[](std::size_t i) { return _data[i]; }
-    const T & operator[](std::size_t i) const { return _data[i]; }
+      // getters
+      T * data() noexcept { return _data.data(); }
+      const T * data() const noexcept { return _data.data(); }
 
-    bool operator<(const Vector<T> &other) const noexcept {
-      return _size < other._size || (_size == other._size && _data < other._data);
-    }
+      std::size_t size() const noexcept {return _size; }
+      std::size_t capacity() const noexcept {return _data.size(); }
 
-    bool operator==(const Vector<T> &other) const noexcept {
-      return !(*this < other) && !(other < *this);
-    }
-  };
+      T& operator[](std::size_t i) { return _data[i]; }
+      const T & operator[](std::size_t i) const { return _data[i]; }
+
+      bool operator<(const Vector<T> &other) const noexcept {
+        return _size < other._size || (_size == other._size && _data < other._data);
+      }
+
+      bool operator==(const Vector<T> &other) const noexcept {
+        return !(*this < other) && !(other < *this);
+      }
+
+    private:
+      std::optional<OwningSpan<T>> resized(const std::size_t size) {
+        if (T *tmp = new (std::nothrow) T[size]; tmp != nullptr) [[likely]] {
+          // move on successful allocation
+          std::uninitialized_move_n(_data.data(), _data.size(), tmp);
+          return OwningSpan(tmp, size);
+        }
+        return std::nullopt;
+      }
+
+      void try_resize() {
+        _data = resized(_size * 2 + 1).value_or(std::move(_data)); // assign new value on success
+      }
+    };
 }
