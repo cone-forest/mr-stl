2000df022a3748ed53a9376b2749ebc4c4c8cccf
Michael Tsukanov (MT6) <73912760+cone-forest@users.noreply.github.com>
2024-08-24 12:46:25 +0300

    Update and validate mr::DynamicRingBuffer interface

--- mr-stl/ringbuf/dynamic_ringbuf.hpp
+++ mr-stl/ringbuf/dynamic_ringbuf.hpp
@@ -1,16 +1,15 @@
 #pragma once
 
-#include <optional>
-#include <vector>
+#include "mr-stl/vector/vector.hpp"
 
 namespace mr {
   template <typename T>
     struct DynamicRingBuffer {
     private:
-      std::vector<T> _data;
-      std::size_t _size;
-      std::size_t _head;
-      std::size_t _tail;
+      mr::Vector<T> _data;
+      std::size_t _size = 0;
+      int _head = 0;
+      int _tail = 0;
 
     public:
       // default constructor
@@ -20,44 +19,91 @@ namespace mr {
       // move constructor
       constexpr DynamicRingBuffer(DynamicRingBuffer &&) noexcept = default;
       // copy assignment operator
-      constexpr DynamicRingBuffer &operator=(const DynamicRingBuffer &) noexcept =
-          default;
+      constexpr DynamicRingBuffer &operator=(const DynamicRingBuffer &) noexcept = default;
       // move assignment operator
-      constexpr DynamicRingBuffer &operator=(DynamicRingBuffer &&) noexcept =
-          default;
+      constexpr DynamicRingBuffer &operator=(DynamicRingBuffer &&) noexcept = default;
       // destructor
       ~DynamicRingBuffer() noexcept = default;
 
-      bool push(T value) {
+      constexpr bool push_back(T value) {
         if (full()) {
-          _data.resize(_size);
+          resize(_head + _size + 3);
         }
 
-        _data[_tail] = value;
-        _tail = (_tail + 1) % _data.size();
+        _data[_tail] = std::move(value);
+        _tail = (_tail + 1) % _data.capacity();
         _size++;
 
         return true;
       }
 
-      constexpr std::optional<T> pop() noexcept {
+      constexpr bool push_front(T value) {
+        if (full()) {
+          resize(_head + _size);
+        }
+
+        _head = (_head + _data.capacity() - 1) % _data.capacity();
+        _data[_head] = std::move(value);
+        _size++;
+
+        return true;
+      }
+
+      constexpr std::optional<T> pop_front() noexcept {
         if (empty()) {
           return std::nullopt;
         }
 
-        T value = _data[_head];
         _head = (_head + 1) % _data.size();
         _size--;
 
-        return value;
+        return std::move(_data[(_head + _data.capacity() - 1) % _data.capacity()]);
       }
 
-      constexpr size_t size() const { return _size; }
+      constexpr std::optional<T> pop_back() noexcept {
+        if (empty()) {
+          return std::nullopt;
+        }
 
-      constexpr size_t capacity() const { return _data.capacity(); }
+        _tail = (_tail + _data.capacity() - 1) % _data.capacity();
+        _size--;
 
-      constexpr bool empty() const { return _size == 0; }
+        return std::move(_data[(_tail + 1) % _data.capacity()]);
+      }
 
-      constexpr bool full() const { return _size == capacity(); }
+      const T & operator[](std::size_t index) const {
+        return _data[(_head + index) % _size];
+      }
+
+      T & operator[](std::size_t index) {
+        return _data[(_head + index) % _size];
+      }
+
+      std::optional<T> at(std::size_t index) const noexcept {
+        if (_size != 0) [[likely]] {
+          return _data[(_head + index) % _size];
+        }
+        return std::nullopt;
+      }
+
+      constexpr std::size_t size() const noexcept { return _size; }
+
+      constexpr std::size_t capacity() const noexcept { return _data.capacity(); }
+
+      constexpr bool empty() const noexcept { return _size == 0; }
+
+      constexpr bool full() const noexcept { return _size == capacity(); }
+
+      constexpr size_t head() const noexcept { return _head; }
+
+      constexpr size_t tail() const noexcept { return _tail; }
+
+      void resize(std::size_t new_size) {
+        _data.resize(new_size);
+        // NOTE: it's probably not worth it to move all data to the beginning
+        std::uninitialized_move_n(begin(_data), _head < _tail ? 0 : _tail,
+                                  begin(_data) + _head + _size - _tail);
+        _tail = _head + _size;
+      }
     };
 }  // namespace mr
